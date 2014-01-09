ada1519d5cd8435ff2f4437e7508786a24b74459
SkyChang <sanc.lcdbest+iservice@gmail.com>
2014-01-09 17:58:25 +0800

    add VaildationNGExtensions

--- src/NGHelper/Properties/AssemblyInfo.cs
+++ src/NGHelper/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // 變更這些屬性的值即可修改組件的相關
 // 資訊。
 [assembly: AssemblyTitle("NGHelper")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Use ASP.NET MVC Htmlhelper create AngularJs validation")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Study4.TW")]
 [assembly: AssemblyProduct("NGHelper")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright © Study4.TW SkyChang 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // 您可以指定所有的值，也可以依照以下的方式，使用 '*' 將組建和修訂編號
 // 指定為預設值: 
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.1")]
+[assembly: AssemblyFileVersion("0.1.0.1")]
---
+++ src/NGHelper/ValidationNGExtensions.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Web.Helpers;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+using System.Web.Mvc.Properties;
+using System.Web.Routing;
+
+namespace NGHelper
+{
+    public static class ValidationNGExtensions
+    {
+
+        public static MvcHtmlString ValidationMessageForNg<TModel, TProperty>
+            (this HtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TProperty>> expression)
+        {
+            return htmlHelper.ValidationMessageForNg<TModel, TProperty>(expression, ((object)null));
+        }
+
+        public static MvcHtmlString ValidationMessageForNg<TModel, TProperty>
+            (this HtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TProperty>> expression,
+            object htmlAttributes)
+        {
+            return htmlHelper.ValidationMessageForNg<TModel, TProperty>(expression,
+                ((IDictionary<string, object>)HtmlHelper.AnonymousObjectToHtmlAttributes(htmlAttributes)));
+        }
+
+        public static MvcHtmlString ValidationMessageForNg<TModel, TProperty>
+            (this HtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TProperty>> expression,
+            IDictionary<string, object> htmlAttributes)
+        {
+            ModelMetadata metadata = ModelMetadata.FromLambdaExpression(expression, htmlHelper.ViewData);
+            var name = ExpressionHelper.GetExpressionText(expression);
+
+            return ValidationMessageNgHelper(htmlHelper, metadata, name, htmlAttributes);
+        }
+
+
+        private static MvcHtmlString ValidationMessageNgHelper(
+            HtmlHelper htmlHelper, ModelMetadata metadata, 
+            string name, IDictionary<string, object> htmlAttributes)
+        {
+            string fullName = htmlHelper.ViewContext.ViewData.TemplateInfo.GetFullHtmlFieldName(name);
+            if (String.IsNullOrEmpty(fullName))
+            {
+                throw new ArgumentException("Object FullName is Null or Empty", "name");
+            }
+
+            TagBuilder divTagBuilder = new TagBuilder("div");
+            divTagBuilder.MergeAttributes(htmlAttributes);
+            divTagBuilder.MergeAttribute("ng-show", string.Format("signup_form.{0}.$dirty && signup_form.Subject.$invalid", fullName));
+            divTagBuilder.AddCssClass("error");
+
+            var validations = ModelValidatorProviders.Providers.GetValidators
+                (metadata ?? ModelMetadata.FromStringExpression(name, new ViewDataDictionary()),
+                new ControllerContext()).SelectMany(v => v.GetClientValidationRules());
+
+            var validatorMessages = validations.ToDictionary
+                (k => k.ValidationType, v => v.ErrorMessage);
+
+            foreach (var item in validatorMessages)
+            {
+                var smallTagBuilder = new TagBuilder("small");
+                smallTagBuilder.AddCssClass("error");
+                switch (item.Key)
+                {
+
+                    case "required":
+                        smallTagBuilder.MergeAttribute("ng-show", string.Format("signup_form.{0}.$error.required", fullName));
+                        smallTagBuilder.SetInnerText(item.Value);
+                        break;
+                    case "length":
+                        smallTagBuilder.MergeAttribute("ng-show",
+                            string.Format("signup_form.{0}.$error.minlength || signup_form.{0}.$error.maxlength", fullName));
+                        smallTagBuilder.SetInnerText(item.Value);
+                        break;
+                    case "url":
+                        smallTagBuilder.MergeAttribute("ng-show", string.Format("signup_form.{0}.$error.url", fullName));
+                        smallTagBuilder.SetInnerText(item.Value);
+                        break;
+                    case "number":
+                        smallTagBuilder.MergeAttribute("ng-show", string.Format("signup_form.{0}.$error.number", fullName));
+                        smallTagBuilder.SetInnerText(item.Value);
+                        break;
+                    case "email":
+                        smallTagBuilder.MergeAttribute("ng-show", string.Format("signup_form.{0}.$error.email", fullName));
+                        smallTagBuilder.SetInnerText(item.Value);
+                        break;
+                    default:
+                        break;
+                }
+                divTagBuilder.InnerHtml += smallTagBuilder.ToString();
+                //ng-pattern="/a-zA-Z/"
+            }
+
+            return new MvcHtmlString(divTagBuilder.ToString());
+        }
+    }
+}
+
+
+
